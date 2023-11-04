a171c921d513e80a7a225c5cdbc7d1267bc852f3
Jort <73478108+jortje@users.noreply.github.com>
2023-11-04 13:24:14 +0100

    3

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, send_file, after_this_request
+from flask import Flask, render_template, request, send_file
 from macrodata import main as generate_macro_data
 import os
-import tempfile
 
 app = Flask(__name__)
 
@@ -11,23 +10,14 @@ def index():
         year = request.form.get('year')
         month = request.form.get('month') or '12'
         try:
-            # Generate the file and store it in a temporary directory
             file_path = generate_macro_data(year, month)
-
-            @after_this_request
-            def remove_file(response):
-                try:
-                    os.remove(file_path)
-                except Exception as error:
-                    app.logger.error("Error removing downloaded file handle", error)
-                return response
-
             return send_file(file_path, as_attachment=True)
         except ValueError as e:
             return render_template('index.html', error=str(e))
         except Exception as e:
-            return render_template('index.html', error="An error occurred while generating the file."), 500
-
+            # Log the error message for debugging purposes
+            print(f"An error occurred: {e}")  # or use logging
+            return render_template('index.html', error="An error occurred while generating the file.")
     return render_template('index.html', error=None)
 
 if __name__ == '__main__':
